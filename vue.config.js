e33c7798bad083c1e05ef3ee179629bacb44841d
Ayub Alam <ayub@agramcloud.com>
2021-10-31 10:37:35 +0545

    pwa

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,19 @@ module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  pwa: {
+    name: 'Agram Infotech',
+    // themeColor: '#2d3748',
+    // msTileColor: '#2d3748',
+    appleMobileWebAppCapable: 'no',
+    appleMobileWebAppStatusBarStyle: 'default',
+    manifestPath: 'manifest.json',
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      swSrc: 'service-worker.js',
+      exclude: ['_redirects'],
+    }
+  },
   pages: {
     index: {
       entry: 'src/main.js',
